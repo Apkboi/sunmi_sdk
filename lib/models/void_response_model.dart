2bac114100d169bfca33be884e06d3054c3d0f84
Victor <abraham.nnu@gmail.com>
2024-10-19 17:29:14 +0100

    Made voided response dynamic

--- lib/models/void_response_model.dart
+++ lib/models/void_response_model.dart
@@ -1,11 +1,11 @@
 class VoidResponse {
-  final String operationId;
-  final String documentNr;
-  final double voidedAmount;
-  final String rrn;
-  final String stan;
-  final String voidStatus;
-  final String receiptText;
+  final dynamic operationId;
+  final dynamic documentNr;
+  final dynamic voidedAmount;
+  final dynamic rrn;
+  final dynamic stan;
+  final dynamic voidStatus;
+  final dynamic receiptText;
 
   VoidResponse({
     required this.operationId,
@@ -21,7 +21,7 @@ class VoidResponse {
     return VoidResponse(
       operationId: json['operationId'],
       documentNr: json['documentNr'],
-      voidedAmount: json['voidedAmount']?.toDouble() ?? 0.0,
+      voidedAmount:json['voidedAmount'] ==null? 0: json['voidedAmount']?.toDouble() ?? 0.0,
       rrn: json['rrn'],
       stan: json['stan'],
       voidStatus: json['voidStatus'],
